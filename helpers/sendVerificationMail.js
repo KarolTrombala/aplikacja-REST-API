2b2a4ce65f1d6990f42d0a7963f4df62cf8b2ba6
Karol Trombala <ktrombala@gmail.com>
2024-08-02 18:32:27 +0200

    mailgun init

---
+++ helpers/sendVerificationMail.js
@@ -0,0 +1,29 @@
+import mgMail from 'mailgun';
+
+export const sendVerificationMail = async (userEmail, verificationToken) => {
+    const baseURL = process.env.BASE_URL;
+    const verificationURL = `${baseURL}/api/users/verify/${verificationToken}`;
+
+    mgMail.setApiKey(process.env.MAILGUN_API_KEY)
+    const msg = {
+        to: userEmail,
+        from: 'ktrombala@gmail.com',
+        subject: 'Email Verification',
+        text: `Click the following link to verify your email: ${verificationURL}`,
+        html: `<strong>
+  Click the following link to verify your email:
+  <a href="${verificationURL}">
+    ${verificationURL}
+  </a>
+</strong>`,
+    }
+
+    mgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
